e16bf4f2f54b6522422abb68a8052b782cc25953
iamyahia <yahiabaiz@gmail.com>
2023-12-01 21:48:02 +0300

    set husky before commiting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import fs from "fs";
-
-console.log("test");
-
-const add = (a: number, b: number) => {
-  console.log(a + b);
-};
-add(1, 4);
-
-fs.writeFileSync("test.txt", "hiii");
+console.log('Tafseerakan');
